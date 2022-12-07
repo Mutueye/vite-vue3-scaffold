9c6b8fba8b07493b91cad9205b8b2f4b74a17233
Mutueye <mutuyueniao@gmail.com>
2022-12-07 18:01:59 +0800

    install @types/node; config env type support; config src alias @

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  // .env中设置的各环境变量，例如VITE_BASE_URL
+  VITE_BASE_URL: string;
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
