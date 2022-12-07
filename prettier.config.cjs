046dcce0af1c976586e5b68b13e6aae91fb55a90
Mutueye <mutuyueniao@gmail.com>
2022-12-07 17:29:28 +0800

    format codes

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,12 +2,12 @@ module.exports = {
   semi: true,
   tabWidth: 2,
   bracketSpacing: true,
-  jsxBracketSameLine: true,
+  bracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'always',
   printWidth: 100,
   htmlWhitespaceSensitivity: 'ignore',
   endOfLine: 'auto',
-  vueIndentScriptAndStyle: true
-}
+  vueIndentScriptAndStyle: true,
+};
