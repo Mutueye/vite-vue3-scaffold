be3ab4b358eaf5221d06064c0a265276a4d9e250
Mutueye <mutuyueniao@gmail.com>
2022-12-08 10:33:36 +0800

    install & config stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: ['stylelint-scss'],
+  extends: [
+    'stylelint-config-standard-scss', // configure for SCSS
+    'stylelint-config-recommended-vue', // add overrides for .Vue files
+    'stylelint-config-recess-order', // use the recess order for properties
+    'stylelint-config-css-modules', // configure for CSS Modules methodology
+    'stylelint-config-prettier', // turn off any rules that conflict with Prettier
+  ],
+  rules: {
+    'color-hex-length': null,
+    'custom-property-empty-line-before': null,
+    'selector-class-pattern': null,
+    'value-keyword-case': null,
+    // 'at-rule-no-unknown': [true, { ignoreAtRules: ['apply'] }],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    // 'function-no-unknown': [true, { ignoreFunctions: ['theme'] }],
+    'scss/dollar-variable-pattern': null,
+    'scss/selector-no-redundant-nesting-selector': true,
+  },
+  ignoreFiles: ['dist/**/*'],
+};
