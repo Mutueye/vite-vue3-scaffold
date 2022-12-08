093587f356411e3d6b124cb5f4aca4ad0351fff6
Mutueye <mutuyueniao@gmail.com>
2022-12-08 10:20:32 +0800

    按照&配置eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: { node: true },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
+  plugins: ['@typescript-eslint'], // might not be needed
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+  },
+  globals: {
+    defineEmits: 'readonly',
+    defineProps: 'readonly',
+  },
+};
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
+  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
