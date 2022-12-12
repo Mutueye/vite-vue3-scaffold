9e7935f9bc8519ad07cdcb4821d1aeefb4e99b58
Mutueye <mutuyueniao@gmail.com>
2022-12-12 22:22:33 +0800

    use line-md animation icons

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     class="w-full h-header border-b-1 border-[var(--el-border-color)] flex flex-row items-center justify-between px-space">
     <div class="text-size-24px font-bold">vite-vue3-scaffold</div>
     <el-button :link="true" type="primary" @click="toggleDark(!isDark)">
-      <i class="dark:i-ion-moon i-ion-sunny text-size-20px" />
+      <i
+        class="dark:i-line-md-moon-filled i-line-md-moon-filled-to-sunny-filled-loop-transition text-size-20px" />
     </el-button>
   </div>
   <RouterView />
