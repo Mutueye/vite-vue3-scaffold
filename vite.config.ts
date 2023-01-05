277af64db76dcdd61d5fd6069958575f3cb58f9a
Mutueye <mutuyueniao@gmail.com>
2023-01-05 18:02:59 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ const baseConfig = defineConfig({
     globals: true,
     setupFiles: [path.resolve(__dirname, 'test/setup.ts')],
   },
-  base: '/<repo>/',
+  base: '/vite-vue3-scaffold/',
 });
 
 /**
