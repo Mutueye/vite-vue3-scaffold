c0cd98a62b97801700616382cd84226aeb5c3917
Mutueye <mutuyueniao@gmail.com>
2023-03-03 10:41:19 +0800

    style

--- src/modules/themeEditor/views/components/perviewer/ComponentSizePreview.vue
+++ src/modules/themeEditor/views/components/perviewer/ComponentSizePreview.vue
@@ -1,12 +1,16 @@
 C
 <template>
   <SectionWrapper title="组件大小" :show-divider="true">
+    <IntroBox
+      content="基于element-plus的配置，提供了三种组件大小，以适应不同的使用场景"
+      class="mt-space-xxs mb-space-xs" />
     <ComponentSizeForm v-for="item in configList" :key="item.cssVar" :config-data="item" />
   </SectionWrapper>
 </template>
 
 <script lang="ts" setup>
   import SectionWrapper from '../SectionWrapper.vue';
+  import IntroBox from './IntroBox.vue';
   import ComponentSizeForm from './ComponentSizeForm.vue';
   import { ThemeCategory } from '@/utils/theme/themeManager';
   import { useThemeConfigList } from '../../composables/useThemeConfigList';
