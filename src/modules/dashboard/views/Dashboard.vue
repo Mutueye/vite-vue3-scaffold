c15f3cc880c5d6add89eb326fa68e6043d44b7e3
Mutueye <mutuyueniao@gmail.com>
2023-05-08 15:36:52 +0800

    clean dashboard

--- src/modules/dashboard/views/Dashboard.vue
+++ src/modules/dashboard/views/Dashboard.vue
@@ -4,7 +4,7 @@
       <div class="w-full py-spacing-xxxl text-center font-bold">HOME</div>
       <HelloWorld msg="Hellow World" />
       <el-button>text</el-button>
-      <div class="p-spacing-sm">
+      <!-- <div class="p-spacing-sm">
         <el-select v-model="selectedSchool" value-key="id">
           <el-option v-for="item in schoolList" :key="item.id" :label="item.name" :value="item" />
         </el-select>
@@ -15,29 +15,29 @@
           <el-table-column property="configKey" label="configKey" />
           <el-table-column property="configValue" label="configValue" />
         </el-table>
-      </div>
+      </div> -->
     </div>
   </ScrollableContentPage>
 </template>
 
 <script lang="ts" setup>
-  import { onMounted, ref } from 'vue';
-  import { get } from 'lodash-es';
+  // import { onMounted, ref } from 'vue';
+  // import { get } from 'lodash-es';
   import HelloWorld from '@/components/HelloWorld.vue';
   import ScrollableContentPage from '@/modules/layout/views/components/ScrollableContentPage.vue';
-  import { ConfigModel, getSchoolList, SchoolModel, getConfigList } from '@/api/dicts';
+  // import { ConfigModel, getSchoolList, SchoolModel, getConfigList } from '@/api/dicts';
 
-  const selectedSchool = ref<SchoolModel>();
-  const schoolList = ref<SchoolModel[]>([]);
+  // const selectedSchool = ref<SchoolModel>();
+  // const schoolList = ref<SchoolModel[]>([]);
 
-  const configList = ref<ConfigModel[]>([]);
+  // const configList = ref<ConfigModel[]>([]);
 
-  onMounted(() => {
-    getSchoolList().then((res) => {
-      schoolList.value = get(res, 'data.data.rows', []);
-    });
-    getConfigList().then((res) => {
-      configList.value = get(res, 'data.data', []);
-    });
-  });
+  // onMounted(() => {
+  //   getSchoolList().then((res) => {
+  //     schoolList.value = get(res, 'data.data.rows', []);
+  //   });
+  //   getConfigList().then((res) => {
+  //     configList.value = get(res, 'data.data', []);
+  //   });
+  // });
 </script>
